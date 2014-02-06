2a4ed43fc68a1e3b1f6d0cd40e870eba7523d605
Pimenov Sergey <spimenov@thevideocafe.com>
2014-02-06 13:04:49 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+#gulp-dotjs-packer
+
+> Compile [doTjs](https://github.com/olado/doT) with gulp (gulpjs.com)
+
+## Information
+
+<table>
+<tr> 
+<td>Package</td><td>gulp-dotjs-packer</td>
+</tr>
+<tr>
+<td>Description</td>
+<td>doTjs compiler for Gulp</td>
+</tr>
+<tr>
+<td>Gulp Version</td>
+<td>3.x</td>
+</tr>
+</table>
+
+## Usage
+
+```javascript
+var gulp = require("gulp");
+var dotPacker = require("gulp-dotjs-packer");
+
+gulp.task('dot', function() {
+  gulp.src('./lib/*.jst')
+	.pipe(dotPacker({
+      fileName: "templates.js"
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
+```
+
+## Options
+
+- `fileName`
+	
+	Name of output file
+
+- `variable`
+
+	Name of variable to store templates.
+
+- `templateSettings`
+
+	doT.js template settings.
+
+- `processName (pathName, base, cwd)`
+
+	Function that replace template name.
+
+## LICENSE
+
+MIT License
+
+Copyright (c) 2014 Pimenov Sergey pimenov.sergei@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,64 @@
+var through = require('through');
+var path = require('path');
+var gutil = require('gulp-util');
+var doT = require('dot');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+
+module.exports = function(opt) {
+    if (!opt) opt = {};
+    if (!opt.fileName) throw new PluginError('gulp-dotjs-packer', 'Missing fileName option for gulp-dotjs-packer');
+    if (!opt.newLine) opt.newLine = gutil.linefeed;
+    if (!opt.variable) opt.variable = "JST";
+    if (!opt.processName) opt.processName = function(pathName, base, cwd) {
+        return pathName.replace(base, "").replace(path.extname(pathName), "");
+    };
+    if (!opt.templateSettings) opt.templateSettings = doT.templateSettings;
+
+    var buffer = [];
+    var firstFile = null;
+    var fileName = opt.fileName;
+    var processName = opt.processName;
+    var variable = opt.variable;
+    var templateSettings = opt.templateSettings;
+
+    function handleCompile(content, settings, defs, variable, path) {
+        var template = "";
+        var compiled = doT.template(content, settings, defs).toString();
+        template += variable + "['" + path + "'] = " + compiled;
+        return template;
+    }
+
+    function proccessContents(contents) {
+        return contents.toString('utf8')
+    }
+
+    function bufferContents(file) {
+        if (file.isNull()) return;
+        if (file.isStream()) return this.emit('error', new PluginError('gulp-dotjs-packer', 'Streaming not supported'));
+
+        if (!firstFile) firstFile = file;
+        buffer.push(handleCompile(proccessContents(file.contents), templateSettings, {}, variable, processName(file.path, file.base, file.cwd)));
+    }
+
+    function endStream() {
+        if (buffer.length === 0) return this.emit('end');
+
+        var joinedContents = buffer.join(opt.newLine);
+        var content = "(function(root, factory) { if (typeof define === 'function' && define.amd) { define(factory); } else { root['" + variable + "'] = factory(); } }(this, function() {\r\nvar " + variable + " = " + variable + " || {}; \r\n" + joinedContents + " \r\nreturn " + variable + "; \r\n}));"
+
+        var joinedPath = path.join(firstFile.base, fileName);
+
+        var joinedFile = new File({
+            cwd: firstFile.cwd,
+            base: firstFile.base,
+            path: joinedPath,
+            contents: new Buffer(content)
+        });
+
+        this.emit('data', joinedFile);
+        this.emit('end');
+    }
+
+    return through(bufferContents, endStream);
+};
